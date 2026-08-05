1cf9cc7aa92fbf855624eadee27aeab3733bad6f
윤중우 <ssanggama99@kaist.ac.kr>
2026-08-05 10:32:07 +0900

    pinia + localstorage

--- src/stores/weatherStore.js
+++ src/stores/weatherStore.js
@@ -2,18 +2,29 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useWeatherStore = defineStore('weather', () => {
-  const cities = ref([])
+  const cities = ref(JSON.parse(localStorage.getItem('weatherCities')) || [])
 
+  const saveCities = () => {
+    localStorage.setItem('weatherCities', JSON.stringify(cities.value))
+  }
   const addCity = (city) => {
     const exists = cities.value.some((item) => item.id === city.id)
 
-    if (!exists) {
-      cities.value.push(city)
+    if (exists) {
+      return false
     }
+
+    cities.value.push(city)
+
+    saveCities()
+
+    return true
   }
 
   const removeCity = (id) => {
     cities.value = cities.value.filter((city) => city.id !== id)
+
+    saveCities()
   }
 
   return {
